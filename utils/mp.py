6b82dbf1b89eb696697c7eec290baa2251bb00a2
Iliya Zhecev <iliya.zhechev@gmail.com>
2020-09-27 00:10:26 +0300

    Refactor mp fit and generator

--- utils/mp.py
+++ utils/mp.py
@@ -3,17 +3,20 @@ import threading
 
 from collections import deque
 import atexit
-from tqdm.auto import trange
+from tqdm.auto import tqdm, trange
 
 
-def fit(model, data_gen, epochs, optim_kw={}):
-    tr = trange(epochs)
-    for e in tr:
-        tr.set_description(f'Epoch {e:03}')
-        fit_generator(model, data_gen, len(data_gen), optim_kw)
+def fit(model, data_gen, epochs=1, its=None, optim_kw={}):
+    if its is None:
+        try:
+            its = len(data_gen)
+        except Exception:
+            pass
 
+    def inf():
+        while True:
+            yield None
 
-def fit_generator(model, data_gen, its, optim_kw={}):
     class FitCTX:
         def __enter__(self):
             self.loss = 999
@@ -34,20 +37,25 @@ def fit_generator(model, data_gen, its, optim_kw={}):
         def _step(self):
             self.model = model
 
-            tr = trange(its)
-            for i in tr:
-                if self.should_terminate:
-                    return
+            tr_ep = trange(epochs)
+            for e in tr_ep:
+                tr_ep.set_description(f'Epoch {e:03}')
 
-                try:
-                    batch = next(data_gen)
-                except StopIteration as _e:
-                    return
+                tr = tqdm(inf(), total=its)
+                tr_it = iter(tr)
+                i = 0
+                while not self.should_terminate:
+                    try:
+                        next(tr_it)
+                        batch = next(data_gen)
+                    except StopIteration as _e:
+                        return
 
-                loss, info = model.optim_step(batch, optim_kw)
-                tr.set_description(f'Loss: {loss:0.6f}')
-                self.loss, self.info = loss, info
-                self.it = i
+                    loss, info = model.optim_step(batch, optim_kw)
+                    tr.set_description(f'Loss: {loss:0.6f}')
+                    self.loss, self.info = loss, info
+                    self.it = i
+                    i += 1
 
         def __exit__(self, exc_type, exc_val, exc_tb):
             self.terminate()
@@ -68,54 +76,76 @@ def fit_generator(model, data_gen, its, optim_kw={}):
     return FitCTX()
 
 
-class MiltiprocessBuffer:
-    def __init__(self, buffer_size, generator_func, num_processes):
-        self.buffer = mp.Queue(maxsize=buffer_size)
-        self.generator_func = generator_func
-        self.lock = mp.Lock()
-        self.processes = [
-            mp.Process(target=self._run, args=(i, ))
-            for i in range(num_processes)
-        ]
+def run_mp_generator(
+    generator_ctor,
+    buffer_size=32,
+    num_processes=1,
+):
+    class MiltiprocessBuffer:
+        def __init__(self):
+            ctx = mp.get_context('fork')  # spawn
+            self.buffer = ctx.Queue(maxsize=buffer_size)
+            self.generator_ctor = generator_ctor
+            self.lock = ctx.Lock()
+            self.processes = [
+                ctx.Process(target=self._run, args=(i, ))
+                for i in range(num_processes)
+            ]
+            self.started = False
 
-        atexit.register(self.terminate)
+            atexit.register(self.terminate)
 
-    def start(self):
-        for p in self.processes:
-            p.start()
+        def __next__(self):
+            if not self.started:
+                self.start()
 
-    def terminate(self):
-        for p in self.processes:
-            if p.is_alive:
-                p.terminate()
+            return self.pop()
 
-    def try_pop(self):
-        # with self.lock:
-        if self.buffer.empty():
-            return None
-        return self.buffer.get()
+        def __iter__(self):
+            return self
 
-    def pop(self):
-        while True:
-            if not self.buffer.empty():
-                return self.buffer.get()
+        def __del__(self):
+            self.terminate()
 
-    def get(self, n):
-        result = []
-        while len(result) < n:
-            if not self.buffer.empty():
-                value = self.buffer.get()
-                result.append(value)
+        def start(self):
+            self.started = True
+            for p in self.processes:
+                p.start()
 
-        return result
+        def terminate(self):
+            for p in self.processes:
+                if p.is_alive:
+                    p.terminate()
 
-    def _run(self, proc_id):
-        generator = self.generator_func()
-        while True:
-            if not self.buffer.full():
-                value = next(generator)
+        def try_pop(self):
+            # with self.lock:
+            if self.buffer.empty():
+                return None
+            return self.buffer.get()
+
+        def pop(self):
+            while True:
+                if not self.buffer.empty():
+                    return self.buffer.get()
+
+        def get(self, n):
+            result = []
+            while len(result) < n:
+                if not self.buffer.empty():
+                    value = self.buffer.get()
+                    result.append(value)
+
+            return result
+
+        def _run(self, proc_id):
+            generator = self.generator_ctor()
+            for value in generator:
+                while self.buffer.full():
+                    pass
                 with self.lock:
                     self.buffer.put(value)
+
+    return MiltiprocessBuffer()
 
 
 def sanity_check():
@@ -129,9 +159,9 @@ def sanity_check():
             val += 1
             yield val
 
-    mpb = MiltiprocessBuffer(
+    mpb = run_mp_generator(
         buffer_size=1000,
-        generator_func=gen_init,
+        generator_ctor=gen_init,
         num_processes=16,
     )
 
@@ -143,51 +173,53 @@ def sanity_check():
 
 
 if __name__ == '__main__':
-    import nn as tu
-    import vis
-    import torch as T
-    import torchvision
-    from torchvision import transforms
-    import matplotlib.pyplot as plt
+    sanity_check()
 
-    def get_mnist_iterator(bs):
-        dataset = torchvision.datasets.MNIST(
-            root='./data',
-            train=True,
-            transform=transforms.Compose([
-                transforms.ToTensor(),
-                torchvision.transforms.Normalize((0.1307,), (0.3081,))
-            ]),
-            download=True
-        )
-        dl = T.utils.data.DataLoader(
-            dataset=dataset,
-            batch_size=bs,
-            # shuffle=True
-        )
-        it = iter(dl)
+    # import nn as tu
+    # import vis
+    # import torch as T
+    # import torchvision
+    # from torchvision import transforms
+    # import matplotlib.pyplot as plt
 
-        while True:
-            try:
-                yield it.next()
-            except StopIteration:
-                it = iter(dl)
+    # def get_mnist_iterator(bs):
+    #     dataset = torchvision.datasets.MNIST(
+    #         root='.data',
+    #         train=True,
+    #         transform=transforms.Compose([
+    #             transforms.ToTensor(),
+    #             torchvision.transforms.Normalize((0.1307,), (0.3081,))
+    #         ]),
+    #         download=True
+    #     )
+    #     dl = T.utils.data.DataLoader(
+    #         dataset=dataset,
+    #         batch_size=bs,
+    #         # shuffle=True
+    #     )
+    #     it = iter(dl)
 
-    X, y = next(get_mnist_iterator(bs=128))
-    ae = tu.DenseAE().to('cuda')
+    #     while True:
+    #         try:
+    #             yield it.next()
+    #         except StopIteration:
+    #             it = iter(dl)
 
-    with vis.fig(figsize=[10, 10]) as ctx, fit(
-        model=ae,
-        its=512,
-        data_gen=([x, x] for x, y in get_mnist_iterator(bs=128)),
-        optim_kw={'lr': 0.001}
-    ) as fit:
-        while not fit.done:
-            imgs = X[:64]
-            pred_imgs = ae(imgs)
+    # X, y = next(get_mnist_iterator(bs=128))
+    # ae = tu.DenseAE().to('cuda')
 
-            # ctx.clear()
-            T.cat([
-                imgs.reshape(1, 1, 8, 8, 1, 28, 28),
-                pred_imgs.reshape(1, 1, 8, 8, 1, 28, 28),
-            ], dim=1).imshow()
+    # with vis.fig(figsize=[10, 10]) as ctx, fit(
+    #     model=ae,
+    #     its=512,
+    #     data_gen=([x, x] for x, y in get_mnist_iterator(bs=128)),
+    #     optim_kw={'lr': 0.001}
+    # ) as fit:
+    #     while not fit.done:
+    #         imgs = X[:64]
+    #         pred_imgs = ae(imgs)
+
+    #         # ctx.clear()
+    #         T.cat([
+    #             imgs.reshape(1, 1, 8, 8, 1, 28, 28),
+    #             pred_imgs.reshape(1, 1, 8, 8, 1, 28, 28),
+    #         ], dim=1).imshow()
